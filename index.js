3d60e0dfc1eb4e12d558f7bf6f225424e4e03e7c
Adelakin Adewumi <61943042+Adelakin-Adewumi@users.noreply.github.com>
2022-06-20 18:09:18 -0700

    javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+let firstName = "Adewumi"
+let lastName = "Adelakin"
+let fullName = `${firstName} ${lastName}`
+let height = 155
+let country = "Nigeria"
+
+let details = `My name is ${fullName}, I am ${height} inches, I am from ${country}`
+alert(details)
